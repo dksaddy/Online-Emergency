80788eaa89cb437e2a2223e2a71f0b781ae3d098
dksaddy <137085811+dksaddy@users.noreply.github.com>
2023-08-11 12:53:08 +0600

    Add files via upload

--- emergencyNumber.php
+++ emergencyNumber.php
@@ -4,15 +4,22 @@ include('db_config.php');
 
 if (isset($_GET['submit'])) {
     $thanaName = $_GET['location'];
-    $sql = "SELECT PhoneNumber FROM thana_number 
+    $sql = "SELECT PhoneNumber,FireService,Ambulance FROM thana_number 
             WHERE LoacationName = '" . $thanaName . "'";
     $result = mysqli_query($conn, $sql);
     $number = "";
+    $fireService = "";
+    $ambulance = "";
 
     if ($result) {
-        while ($row = mysqli_fetch_array($result, MYSQLI_NUM))
+        while ($row = mysqli_fetch_array($result, MYSQLI_NUM)) {
             $number = $row[0];
-    }
+            $fireService = $row[1];
+            $ambulance = $row[2];
+        }
+           
+
+    }//if condition end
 }
 
 ?>
@@ -31,21 +38,47 @@ if (isset($_GET['submit'])) {
 <body>
     <form action="" method="GET" class="area">
         <input type="text" name="location" class="text-field">
-        <input type="submit" value="Thana Number" name="submit" class="submit-button">
+        <input type="submit" value="Number" name="submit" class="submit-button">
     </form>
 
     <br>
     <div class="numberArea">
+        <label class="thanaNumberLabel">Police Station:</label>
         <?php
-        if ($number == "")
+         if ($number == "")
             echo "<p class=pera>No Number Found!</p>";
-        else
+         else
             echo "<button class=numberButton>
             <a class=tel-link href=tel:>" . $number . "</a> 
             </button>";
         ?>
     </div>
 
+    <br>
+    <div class="numberArea">
+        <label class="fireserviceLabel">Fire Service:</label>
+        <?php
+         if ($fireService == "")
+            echo "<p class=pera>No Number Found!</p>";
+         else
+            echo "<button class=numberButton>
+            <a class=tel-link href=tel:>" . $fireService . "</a> 
+            </button>";
+        ?>
+    </div>
+
+    <br>
+    <div class="numberArea">
+        <label class="ambulanceLabel">Ambulance:</label>
+        <?php
+         if ($ambulance == "")
+            echo "<p class=pera>No Number Found!</p>";
+         else
+            echo "<button class=numberButton>
+            <a class=tel-link href=tel:>" . $ambulance . "</a> 
+            </button>";
+        ?>
+    </div>
 
 
 
